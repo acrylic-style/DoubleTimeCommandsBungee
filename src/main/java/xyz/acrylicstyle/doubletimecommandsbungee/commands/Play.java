c528a20ff007373e81c35b320c1b7c086c52fd2e
acrylic-style <me@acrylicstyle.xyz>
2019-11-03 21:35:39 +0900

    pre

--- src/main/java/xyz/acrylicstyle/doubletimecommandsbungee/commands/Play.java
+++ src/main/java/xyz/acrylicstyle/doubletimecommandsbungee/commands/Play.java
@@ -2,18 +2,10 @@ package xyz.acrylicstyle.doubletimecommandsbungee.commands;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
-import util.Collection;
-import xyz.acrylicstyle.doubletimecommandsbungee.DoubleTimeCommands;
 import xyz.acrylicstyle.doubletimecommandsbungee.utils.Utils;
-
-import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Play extends Command {
     public Play() {
